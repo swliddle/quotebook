f1d5e776be230426ec52b6902ddcb0c48c925546
Steve Liddle <liddle@byu.edu>
2016-09-07 14:01:42 -0600

    Initial Commit

---
+++ Founding Fathers Quote Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Steve Liddle on 9/7/16.
+//  Copyright © 2016 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
