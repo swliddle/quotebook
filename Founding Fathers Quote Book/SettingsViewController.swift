5c51a900dd1855d0f6a075d47b1eabbc3655bdc2
Steve Liddle <liddle@byu.edu>
2016-09-08 17:22:26 -0600

    Implemented first-cut picker view data source for selecting notification time.

---
+++ Founding Fathers Quote Book/SettingsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Steve Liddle on 9/8/16.
+//  Copyright © 2016 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController : UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+            case 0:
+                return 12
+            case 1:
+                return 12
+            case 2:
+                return 2
+            default:
+                return 0
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return String(row + 1)
+        case 1:
+            return String(row * 5)
+        case 2:
+            return row <= 0 ? "AM" : "PM"
+        default:
+            return nil
+        }
+    }
+}
