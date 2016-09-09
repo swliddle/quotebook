ee12c0c233f804fc098588a5474e761393764d54
Steve Liddle <liddle@byu.edu>
2016-09-09 14:51:31 -0600

    Committing cumulative updates as I push this all to github.

--- Founding Fathers Quote Book/SettingsViewController.swift
+++ Founding Fathers Quote Book/SettingsViewController.swift
@@ -9,34 +9,132 @@
 import UIKit
 
 class SettingsViewController : UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    // MARK: - Constants
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 3
+    private struct Component {
+        static let Hours = 0
+        static let Minutes = 1
+        static let AmPm = 2
+    }
+
+    private struct Picker {
+        static let AmPmCount = 2
+        static let AM = "AM"
+        static let ComponentWidth: CGFloat = 50.0
+        static let InitialHourIndex = 6
+        static let MinutesPerGroup = 5
+        static let NumberOfHours = 12
+        static let NumberOfMinuteElements = 12
+        static let PM = "PM"
+        static let RowHeight: CGFloat = 30.0
+        static let WheelFactor = 24
+    }
+
+    private struct Color {
+        static let disabled = UIColor.clear
+        static let enabled = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
+    }
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var notificationsSwitch: UISwitch!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet var dayButtons: [UIButton]!
+
+    // MARK: - Properties
+    
+    var notificationsOn = true
+    var hourIndex = Picker.InitialHourIndex
+    var minutesIndex = 0
+    var isAm = true
+    var notifyDays = [ true, true, true, true, true, true, true ]
+    
+    // MARK: - View controller lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateUI()
+    }
+
+    // MARK: - Helpers
+    
+    func updateUI() {
+        notificationsSwitch.setOn(notificationsOn, animated: false)
+
+        pickerView.selectRow(hourIndex + Picker.WheelFactor * Picker.NumberOfHours,
+                             inComponent: Component.Hours, animated: false)
+        pickerView.selectRow(minutesIndex + Picker.WheelFactor * Picker.NumberOfMinuteElements,
+                             inComponent: Component.Minutes, animated: false)
+        pickerView.selectRow(isAm ? 0 : 1, inComponent: Component.AmPm, animated: false)
+        
+        for button in dayButtons {
+            button.backgroundColor = notifyDays[button.tag] ? Color.enabled : Color.disabled
+        }
     }
     
+    // MARK: - Actions
+    
+    @IBAction func toggleNotifications(_ sender: UISwitch) {
+        notificationsOn = sender.isOn
+    }
+    
+    @IBAction func toggleDay(_ sender: UIButton) {
+        notifyDays[sender.tag] = !notifyDays[sender.tag]
+        updateUI()
+    }
+    
+    // MARK: - Picker data source
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return Component.AmPm + 1
+    }
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
-            case 0:
-                return 12
-            case 1:
-                return 12
-            case 2:
-                return 2
+            case Component.Hours:
+                return Picker.NumberOfHours * (Picker.WheelFactor * 2 + 1)
+            case Component.Minutes:
+                return Picker.NumberOfMinuteElements * (Picker.WheelFactor * 2 + 1)
+            case Component.AmPm:
+                return Picker.AmPmCount
             default:
                 return 0
         }
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         switch component {
-        case 0:
-            return String(row + 1)
-        case 1:
-            return String(row * 5)
-        case 2:
-            return row <= 0 ? "AM" : "PM"
+        case Component.Hours:
+            return String(row % Picker.NumberOfHours + 1)
+        case Component.Minutes:
+            return String(format: "%02d", row % Picker.NumberOfMinuteElements * Picker.MinutesPerGroup)
+        case Component.AmPm:
+            return row <= 0 ? Picker.AM : Picker.PM
         default:
             return nil
         }
+    }
+    
+    // MARK: - Picker delegate
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component != Component.AmPm {
+            let rowCount = component == Component.Hours ? Picker.NumberOfHours : Picker.NumberOfMinuteElements
+            if row < Picker.WheelFactor * rowCount || row >= (Picker.WheelFactor + 1) * rowCount {
+
+                pickerView.selectRow(row % rowCount + Picker.WheelFactor * rowCount,
+                                     inComponent: component, animated: false)
+            }
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return Picker.RowHeight
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return Picker.ComponentWidth
     }
 }
--- Founding Fathers Quote Book/TopicsViewController.swift
+++ Founding Fathers Quote Book/TopicsViewController.swift
@@ -9,31 +9,30 @@
 import UIKit
 
 class TopicsViewController : UITableViewController {
-
+    
     // MARK: - Properties
-
+    
     let topics = [ "achieving", "action", "attitude", "character", "confession", "duty",
                    "excuses", "facts", "goal", "guilt", "happiness", "honor", "lies",
                    "lying", "motivational", "philosophy", "politics", "reputation",
                    "stubborn", "truth", "war", "wishes" ];
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return topics.count
     }
-
-    override func tableView(_ tableView: UITableView,
-                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell")!
-
+        
         cell.textLabel?.text = topics[indexPath.row].capitalized
-
+        
         return cell
     }
-
+    
     // MARK: - Table view delegate
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ShowQuote", sender: self)
     }
