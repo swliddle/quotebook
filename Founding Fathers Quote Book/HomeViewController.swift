27db07acdad18ae7dabee17edc97befe3c02822e
Steve Liddle <liddle@byu.edu>
2016-09-12 12:58:40 -0600

    Fixed auto-rotate upside down portrait condition.

---
+++ Founding Fathers Quote Book/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Steve Liddle on 9/12/16.
+//  Copyright © 2016 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController : UIViewController {
+}
