d5394738b25c96826276a00f34ae187fd07b3ed8
Steve Liddle <liddle@byu.edu>
2016-09-16 07:51:53 -0600

    Added CSS-based animation to the quote/speaker transition and CoreAnimation animation to title change.

--- Founding Fathers Quote Book/QuoteViewController.swift
+++ Founding Fathers Quote Book/QuoteViewController.swift
@@ -75,7 +75,7 @@ class QuoteViewController : UIViewController {
             }
         }
 
-        updateUI()
+        updateUIByToggling()
     }
     
     // MARK: - Helpers
@@ -111,13 +111,71 @@ class QuoteViewController : UIViewController {
         return Calendar.current.date(byAdding: dayComponent, to: date)!
     }
 
+    private func updateUIByToggling() {
+        let quote = quotes[currentQuoteIndex]
+        
+        if let topicFilter = topic {
+            // See http://bit.ly/2ctUdTI
+            
+            let fadeTextAnimation = CATransition()
+            fadeTextAnimation.duration = 0.75
+            fadeTextAnimation.type = kCATransitionFade
+            
+            navigationController?.navigationBar.layer.add(fadeTextAnimation, forKey: "fadeText")
+            navigationItem.title = "\(topicFilter.capitalized) (\(currentQuoteIndex + 1) of \(quotes.count))"
+        }
+
+        webView.stringByEvaluatingJavaScript(from: "toggleQuote('\(quote.text)', '\(quote.speaker)')")
+    }
+
     private func updateUI() {
         let quote = quotes[currentQuoteIndex]
         
         if let topicFilter = topic {
             title = "\(topicFilter.capitalized) (\(currentQuoteIndex + 1) of \(quotes.count))"
         }
-        
-        webView.loadHTMLString("<!DOCTYPE html><html><head><title>Quote of the Day</title><style>body { padding: 2em; } .quote { font-size: 24pt; font-style: italic; } .speaker { padding-top: 1.5em; text-align: right; }</style></head><body><div class=\"quote\">\(quote.text)</div><div class=\"speaker\">&mdash; \(quote.speaker)</div></body></html>", baseURL: nil)
+
+        webView.loadHTMLString(
+            "<!DOCTYPE html>" +
+            "<html>" +
+                "<head>" +
+                    "<title>Quote of the Day</title>" +
+                    "<style>" +
+                        "body { position: relative; } " +
+                        ".quote { font-size: 24pt; font-style: italic; } " +
+                        ".speaker { font-size: 18pt; padding-top: 1.5em; text-align: right; }" +
+                        ".container { position: absolute; padding: 2em; left: 0; top: 0; transition: opacity 1s ease-in-out; }" +
+                        "#quote1 { opacity: 1; }" +
+                        "#quote2 { opacity: 0; }" +
+                    "</style>" +
+                    "<script type=\"text/javascript\">" +
+                        "function toggleQuote(quote, speaker) {" +
+                            "var q1 = document.getElementById('quote1');" +
+                            "var q2 = document.getElementById('quote2');" +
+                            "var style = window.getComputedStyle(q1);" +
+                            "var t1 = q1, t2 = q2;" +
+                            "if (style.opacity <= 0) {" +
+                                "t1 = q2;" +
+                                "t2 = q1;" +
+                            "}" +
+                            "t2.getElementsByClassName('quote')[0].innerHTML = quote;" +
+                            "t2.getElementsByClassName('speaker')[0].innerHTML = '&mdash; ' + speaker;" +
+                            "t1.style.opacity = 0;" +
+                            "t2.style.opacity = 1;" +
+                        "}" +
+                    "</script>" +
+                "</head>" +
+                "<body>" +
+                    "<div id=\"quote1\" class=\"container\">" +
+                        "<div class=\"quote\">\(quote.text)</div>" +
+                        "<div class=\"speaker\">&mdash; \(quote.speaker)</div>" +
+                    "</div>" +
+                    "<div id=\"quote2\" class=\"container\">" +
+                        "<div class=\"quote\"></div>" +
+                        "<div class=\"speaker\"></div>" +
+                    "</div>" +
+                "</body>" +
+            "</html>",
+            baseURL: nil)
     }
 }
