ee29778fbff9c5a0c2e10751bbf46c94d0e333e5
Steve Liddle <liddle@byu.edu>
2017-09-13 12:51:52 -0600

    Initial commit.  UI prototype is essentially complete.

---
+++ Founding Fathers Quote Book/QuoteViewController.swift
@@ -0,0 +1,53 @@
+//
+//  QuoteViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Steve Liddle on 9/13/17.
+//  Copyright © 2017 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class QuoteViewController : UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.loadHTMLString("""
+                                <!DOCTYPE html>
+                                <html>
+                                <head>
+                                <title>Quote of the Day</title>
+                                <meta name="viewport" content="initial-scale=1.0">
+                                <style>
+                                    body {
+                                        padding: 1em;
+                                        font-size: 24pt;
+                                    }
+
+                                    .quote {
+                                        font-style: italic;
+                                    }
+
+                                    .speaker {
+                                        text-align: right;
+                                        padding-top: 1em;
+                                    }
+
+                                    .speaker::before {
+                                        content: "— ";
+                                    }
+                                </style>
+                                </head>
+                                <body>
+                                <div class="quote">Do you want to know who you are? Don&rsquo;t ask. Act!
+                                                   Action will delineate and define you.</div>
+                                <div class="speaker">Thomas Jefferson</div>
+                                </body>
+                                </html>
+                               """, baseURL: nil)
+    }
+}
