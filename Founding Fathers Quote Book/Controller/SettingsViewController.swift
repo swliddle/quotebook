76011534fc82c59ac6992f21ed8bd8bbfaa0829f
Steve Liddle <liddle@byu.edu>
2017-09-14 17:00:01 -0600

    Finished Part 14, settings picker view configuration.

---
+++ Founding Fathers Quote Book/Controller/SettingsViewController.swift
@@ -0,0 +1,147 @@
+//
+//  SettingsViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Steve Liddle on 9/14/17.
+//  Copyright © 2017 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController : UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // MARK: - Constants
+    
+    private struct Color {
+        static let disabled = UIColor.clear
+        static let enabled = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
+    }
+    
+    private struct Component {
+        static let Hours = 0
+        static let Minutes = 1
+        static let AmPm = 2
+    }
+    
+    private struct Picker {
+        static let AmPmCount = 2
+        static let AM = "AM"
+        static let ComponentWidth: CGFloat = 50.0
+        static let InitialHourIndex = 6
+        static let NoonIndex = 11
+        static let NumberOfHours = 12
+        static let NumberOfMinutes = 60
+        static let PM = "PM"
+        static let RowHeight: CGFloat = 30.0
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var notificationsSwitch: UISwitch!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet var dayButtons: [UIButton]!
+    
+    // MARK: - Properties
+    
+    var notificationsOn = true
+    var hourIndex = Picker.InitialHourIndex
+    var minutesIndex = 0
+    var isAm = true
+    var notifyDays = [ true, true, true, true, true, true, true ]
+    
+    // MARK: - View controller lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func saveSettings() {
+        // NEEDSWORK: do it
+    }
+
+    private func updateUI() {
+        notificationsSwitch.setOn(notificationsOn, animated: false)
+
+        pickerView.selectRow(hourIndex, inComponent: Component.Hours, animated: false)
+        pickerView.selectRow(minutesIndex, inComponent: Component.Minutes, animated: false)
+        pickerView.selectRow(isAm ? 0 : 1, inComponent: Component.AmPm, animated: false)
+
+        for button in dayButtons {
+            button.backgroundColor = notifyDays[button.tag] ? Color.enabled : Color.disabled
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func toggleNotifications(_ sender: UISwitch) {
+        notificationsOn = sender.isOn
+        saveSettings()
+    }
+    
+    @IBAction func toggleDay(_ sender: UIButton) {
+        notifyDays[sender.tag] = !notifyDays[sender.tag]
+        updateUI()
+        saveSettings()
+    }
+    
+    // MARK: - Picker data source
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return Component.AmPm + 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case Component.Hours:
+            return Picker.NumberOfHours
+        case Component.Minutes:
+            return Picker.NumberOfMinutes
+        case Component.AmPm:
+            return Picker.AmPmCount
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case Component.Hours:
+            return "\(row + 1)"
+        case Component.Minutes:
+            return String(format: "%02d", row)
+        case Component.AmPm:
+            return row <= 0 ? Picker.AM : Picker.PM
+        default:
+            return nil
+        }
+    }
+    
+    // MARK: - Picker delegate
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case Component.Hours:
+            hourIndex = row
+        case Component.Minutes:
+            minutesIndex = row
+        case Component.AmPm:
+            isAm = row <= 0 ? true : false
+        default:
+            break
+        }
+        
+        saveSettings()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return Picker.RowHeight
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return Picker.ComponentWidth
+    }
+}
