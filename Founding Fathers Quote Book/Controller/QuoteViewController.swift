bf9504a76910595309e27a29a6057f768da5ff88
Steve Liddle <liddle@byu.edu>
2017-09-14 14:12:01 -0600

    Finished Part 10, refine Quote VC.

--- Founding Fathers Quote Book/Controller/QuoteViewController.swift
+++ Founding Fathers Quote Book/Controller/QuoteViewController.swift
@@ -11,13 +11,24 @@ import WebKit
 
 class QuoteViewController : UIViewController {
     
+    // MARK: - Constants
+    
+    private struct Storyboard {
+        static let QuoteOfTheDayTitle = "Quote of the Day"
+        static let ShowTopicsSegueIdentifier = "ShowTopics"
+        static let TodayTitle = "Today"
+        static let TopicsTitle = "Topics"
+    }
+    
     // MARK: - Outlets
     
+    @IBOutlet weak var toggleButton: UIBarButtonItem!
     @IBOutlet weak var webView: WKWebView!
     
     // MARK: - Properties
     
     var currentQuoteIndex = 0
+    var quotes: [Quote]!
     var topic: String?
     
     // MARK: - View controller lifecycle
@@ -25,8 +36,22 @@ class QuoteViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        chooseQuoteOfTheDay()
-        updateUI()
+        configure()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func showQuoteOfTheDay() {
+        topic = nil
+        configure()
+    }
+    
+    @IBAction func toggleTopics(_ sender: UIBarButtonItem) {
+        if sender.title == Storyboard.TopicsTitle {
+            performSegue(withIdentifier: Storyboard.ShowTopicsSegueIdentifier, sender: sender)
+        } else {
+            showQuoteOfTheDay()
+        }
     }
     
     // MARK: - Helpers
@@ -40,8 +65,28 @@ class QuoteViewController : UIViewController {
         }
     }
 
+    private func configure() {
+        if let currentTopic = topic {
+            quotes = QuoteDeck.sharedInstance.quotesForTag(currentTopic)
+            currentQuoteIndex = 0
+        } else {
+            quotes = QuoteDeck.sharedInstance.quotes
+            chooseQuoteOfTheDay()
+        }
+
+        updateUI()
+    }
+
     private func updateUI() {
-        let currentQuote = QuoteDeck.sharedInstance.quotes[currentQuoteIndex]
+        let currentQuote = quotes[currentQuoteIndex]
+
+        if let currentTopic = topic {
+            toggleButton.title = Storyboard.TodayTitle
+            title = "\(currentTopic.capitalized) (\(currentQuoteIndex + 1) of \(quotes.count))"
+        } else {
+            toggleButton.title = Storyboard.TopicsTitle
+            title = Storyboard.QuoteOfTheDayTitle
+        }
 
         webView.loadHTMLString(currentQuote.htmlPage(), baseURL: nil)
     }
@@ -49,12 +94,10 @@ class QuoteViewController : UIViewController {
     // MARK: - Segues
     
     @IBAction func exitModalScene(_ segue: UIStoryboardSegue) {
-        // Nothing to do; just need a target for the unwind segue
+        // In this case, there is nothing to do, but we need a target
     }
     
     @IBAction func showTopicQuotes(_ segue: UIStoryboardSegue) {
-        if let currentTopic = topic {
-            title = currentTopic
-        }
+        configure()
     }
 }
